14f8481250bbd8cccdfc2e0f2c34c7cb51ed6ad7
Will Rodman <willc.rodman@gmail.com>
2022-03-09 00:00:02 +0000

    .

--- main.py
+++ main.py
@@ -46,13 +46,41 @@ def pad(x,y):
 
 
 def subquadratic_multiply(x, y):
-    ### TODO
-    pass
-    ###
+
+    xvec = x.binary_vec
+    yvec = y.binary_vec
+
+    print(xvec, yvec)
+
+    if x.decimal_val <= 1 and y.decimal_val <= 1:
+      return BinaryNumber(x.decimal_val * y.decimal_val)
+
+    xvec, yvec = pad(xvec, yvec)
+
+    n = len(xvec)
+  
+    xL, xR = split_number(xvec)
+    yL, yR = split_number(yvec)
+
+    left = subquadratic_multiply(xL, yL)
+  
+    mid_left = subquadratic_multiply(xL, yR)
+    mid_right = subquadratic_multiply(xR, yL)
+  
+    mid = BinaryNumber(mid_left.decimal_val + mid_right.decimal_val)
+    mid = bit_shift(mid, len(xvec)//2) 
+
+    left = bit_shift(left, len(xvec))
+  
+    right = subquadratic_multiply(xR, yR)
+
+    return BinaryNumber(left.decimal_val + mid.decimal_val + right.decimal_val)
+  
 
 ## Feel free to add your own tests here.
 def test_multiply():
     assert subquadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+    assert subquadratic_multiply(BinaryNumber(5), BinaryNumber(7)) == 5*7
 
 def time_multiply(x, y, f):
     start = time.time()
